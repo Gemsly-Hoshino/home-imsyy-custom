4abb001dcb394c800ce4bbf370c5a0dd0cc6298d
gemsly <gemsly@163.com>
2026-01-31 14:26:24 +0800

    chang url

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -53,7 +53,7 @@ const siteIcp = ref(import.meta.env.VITE_SITE_ICP);
 const siteAuthor = ref(import.meta.env.VITE_SITE_AUTHOR);
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "https://blog.harrverse.com";
+  if (!url) return "https://blog.gemslyho.org";
   // 判断协议前缀
   if (!url.startsWith("http://") && !url.startsWith("https://")) {
     return "//" + url;
